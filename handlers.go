58f8adf8a8aa48fe7813f5ec423a1e7344f1c654
jonathan.jiang <jonathan.jiang@skylinetw.com>
2021-08-02 15:59:54 +0800

    Added some error handlings

--- handlers.go
+++ handlers.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-// Struct (our models)
+// Struct (our user)
 type User struct {
 	ID  string `json:"user_id"`
 	AGE int32  `json:"age"`
@@ -22,18 +21,22 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 
 	for _, user := range users {
 		if user.ID == params["user_id"] {
-			fmt.Println(params["user_id"])
 			json.NewEncoder(w).Encode(user)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&User{})
+	// No user found
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user User
 	_ = json.NewDecoder(r.Body).Decode(&user)
+	if user.AGE < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	users = append(users, user)
 
 	w.WriteHeader(http.StatusNoContent)
@@ -46,8 +49,8 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 	for index, user := range users {
 		if user.ID == params["user_id"] {
 			users = append(users[:index], users[index+1:]...)
-			break
+			w.WriteHeader(http.StatusNoContent)
+			return
 		}
 	}
-	w.WriteHeader(http.StatusNoContent)
 }

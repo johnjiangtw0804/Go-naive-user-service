91a992d3621c9a3c1798478f0f9c4377c2d03e08
jonathan.jiang <jonathan.jiang@skylinetw.com>
2021-08-02 13:47:25 +0800

    changed port number

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ func main() {
 	// type inference :=
 	r := mux.NewRouter()
 	// Route handler / Endpoints
+	log.Println("Server starting")
 	r.HandleFunc("/user/{user_id}", getUser).Methods("GET")
 	r.HandleFunc("/user", createUser).Methods("POST")
 	r.HandleFunc("/user/{user_id}", deleteUser).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }

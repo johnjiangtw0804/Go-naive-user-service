7f2fa4cb1a8c1fa07dbd3f09577f44705b81b167
Jonathan.Jiang <johnjiangtw0804@gmail.com>
2021-08-02 23:15:50 +0800

    interface for storage added

---
+++ storage.go
@@ -0,0 +1,28 @@
+package main
+
+// StorageType variable type defines available storage types
+// 0 is memory and etc...
+type StorageType int
+
+// StorageType option
+const Memory StorageType = 0
+
+// type interface for different database to implement
+type Storage interface {
+	GetUser(id string) (User, error)
+	CreateUser(User) error
+	DeleteUser(id string) error
+}
+
+// Assigning the storage space according to user's input
+func NewStorage(storageType StorageType) (Storage, error) {
+	var database Storage
+	var err error
+
+	switch storageType {
+	case Memory:
+		database = new(StorageMemory)
+	}
+
+	return database, err
+}
